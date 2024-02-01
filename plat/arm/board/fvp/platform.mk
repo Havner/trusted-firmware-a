6133a57a47fd7d75197f765adc3bcd7a103c6cfa
Lukasz Pawelczyk <l.pawelczyk@samsung.com>
2024-02-01 16:06:09 +0100

    Restore PSA/RSS usage for attestation in PLAT=fvp for BL31
    Latest TF-A seems to have removed that completely. Strange, I thought that FVP
    with full support for CCA with cortex-a and cortex-m was supposed to be released
    at some point.

--- plat/arm/board/fvp/platform.mk
+++ plat/arm/board/fvp/platform.mk
@@ -474,9 +474,13 @@ BL2_SOURCES		+=	plat/arm/board/fvp/fvp_common_measured_boot.c	\
 
 ifneq (${PLAT_RSS_COMMS_USE_SERIAL},0)
     $(warning "RSS serial used, need an external HES implementation.")
-    BL1_CFLAGS		+=	-DPLAT_RSS_COMMS_USE_SERIAL=1
-    BL2_CFLAGS		+=	-DPLAT_RSS_COMMS_USE_SERIAL=1
-    BL31_CFLAGS		+=	-DPLAT_RSS_COMMS_USE_SERIAL=1
+    BL31_CFLAGS += -DPLAT_RSS_COMMS_USE_SERIAL=1 \
+                   -DPLAT_RSS_COMMS_PAYLOAD_MAX_SIZE=0x1000 \
+                   -DENABLE_CONSOLE_GETC=1
+    include drivers/arm/rss/rss_comms.mk
+    BL31_SOURCES += lib/psa/measured_boot.c \
+                    lib/psa/delegated_attestation.c \
+                    ${RSS_COMMS_SOURCES}
 endif
 endif
 
